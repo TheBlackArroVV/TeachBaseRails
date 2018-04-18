81b99b3dffb529658d85848b688874f390b89bad
TheBlackArroVV <theblackarrovv@gmail.com>
2018-04-18 12:04:45 +0300

    db fixes

--- db/migrate/20180418083704_add_indexes_to_railway_stations_routes.rb
+++ db/migrate/20180418083704_add_indexes_to_railway_stations_routes.rb
@@ -1,6 +1,6 @@
 class AddIndexesToRailwayStationsRoutes < ActiveRecord::Migration[5.1]
   def change
-    add_index :railway_stations_routes, :railwaystation_id
+    add_index :railway_stations_routes, :railway_station_id
     add_index :railway_stations_routes, :route_id
   end
 end
